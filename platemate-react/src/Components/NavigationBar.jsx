4ac963158ba6618fd20138333df75dc81a1c2f8f
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-03-12 00:00:30 +0000

    add conditional rendering on navbar buttons

--- platemate-react/src/Components/NavigationBar.jsx
+++ platemate-react/src/Components/NavigationBar.jsx
@@ -4,14 +4,16 @@ import { Auth } from './Auth';
 
 
 export default function NavigationBar({ loggedIn, setLoggedIn }) {
+
   return (
     <div className='bg-conifer-950 p-2'>
       <nav>
         <ul className='flex justify-between items-center'>
-          <li> <Link className='btn bg-conifer-700 hover:bg-conifer-800 active:bg-conifer-900' to={`Home`}>Home</Link></li>
-          <li> <Link className='btn bg-conifer-700 hover:bg-conifer-800 active:bg-conifer-900' to={`Calendar`}>Calendar</Link></li>
+          <li> <Link className='btn bg-conifer-700 hover:bg-conifer-800 active:bg-conifer-900' to={loggedIn ? "Home" : "/"}>Home</Link></li>
+        {loggedIn && <> <li> <Link className='btn bg-conifer-700 hover:bg-conifer-800 active:bg-conifer-900' to={`Calendar`}>Calendar</Link></li>
           <li> <Link className='btn bg-conifer-700 hover:bg-conifer-800 active:bg-conifer-900' to={`Chat`}>Chat</Link></li>
-          <li> <Link className='btn bg-conifer-700 hover:bg-conifer-800 active:bg-conifer-900' to={`UploadMeal`}>Upload Meal</Link></li>
+          <li> <Link className='btn bg-conifer-700 hover:bg-conifer-800 active:bg-conifer-900' to={`UploadMeal`}>Upload Meal</Link></li></> }
+        
           <li><Auth loggedIn={loggedIn} setLoggedIn={setLoggedIn} /></li>
         </ul>
       </nav>
