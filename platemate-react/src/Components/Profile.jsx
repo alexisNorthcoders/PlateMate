46ee8aaa00c0fe7b02eb9e56e71fb8fc63ea6b02
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-03-13 01:15:22 +0000

    add  react-firebase-hooks dependency, add useAuthState on Profile

--- platemate-react/src/Components/Profile.jsx
+++ platemate-react/src/Components/Profile.jsx
@@ -1,35 +1,47 @@
 import React, { useEffect, useState } from 'react';
-import { database } from "../config/firebase";
+import { database,auth } from "../config/firebase";
 import {  ref,get } from 'firebase/database';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 const Profile = () => {
     const [data, setData] = useState(null);
+    const [user, loading, error] = useAuthState(auth);
 
     useEffect(() => {
-       
-        const dbRef = ref(database, 'users');
-        get(dbRef)
-          .then((snapshot) => {
-           
-            setData(snapshot.val());
-          })
-          .catch((error) => {
-            
-            console.error('Error fetching data:', error);
-          });
-      }, [database]);
+        if (user) { 
+            const dbRef = ref(database, 'users');
+            get(dbRef)
+                .then((snapshot) => {
+                    setData(snapshot.val());
+                })
+                .catch((error) => {
+                    console.error('Error fetching data:', error);
+                });
+        }
+    }, [user]);
     
-    
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
+    if (!user) {
+        return <div>Please sign in to view your profile.</div>;
+    }
+
     return (
         <section className="bg-conifer-900 text-white py-16 flex flex-col">
             <h1>Profile</h1>
-           <p>Name</p>
-           <p>Description / allergens</p>
-           <p>Avatar picture</p>
-           <p> Current Meal</p>
-           <p> Previous Meals</p>
-           <p> Requests notifications</p>
-           {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+            <p>Name</p>
+            <p>Description / allergens</p>
+            <p>Avatar picture</p>
+            <p>Current Meal</p>
+            <p>Previous Meals</p>
+            <p>Requests notifications</p>
+            {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
         </section>
     );
 };
